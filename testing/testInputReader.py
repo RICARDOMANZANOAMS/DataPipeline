625a85823774d6f3bf47bca25534b9f5b2a0f0ea
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-10-15 23:48:19 -0400

    Add testCsvReaderFolder

--- testing/testInputReader.py
+++ testing/testInputReader.py
@@ -34,7 +34,19 @@ class testInputReader(unittest.TestCase):
         pd.testing.assert_frame_equal(result, df_expected)
         os.remove(tmp_path)    
 
-    
+    def testCsvReaderFolder(self):   
+        from inputReader import csvReaderFolder     
+        df1 = pd.DataFrame({"a": [3, 4], "b": [5, 6]})
+        df2 = pd.DataFrame({"a": [6, 7], "b": [8, 9]})       
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            path1 = os.path.join(tmp_dir, "file1.csv")
+            path2 = os.path.join(tmp_dir, "file2.csv")
+            df1.to_csv(path1, index=False)
+            df2.to_csv(path2, index=False)            
+            csv_folder_reader = csvReaderFolder()
+            result_df = csv_folder_reader.readInput(tmp_dir)
+            expected_df = pd.concat([df1, df2], ignore_index=True)
+            pd.testing.assert_frame_equal(result_df.reset_index(drop=True), expected_df)
 
 
 if __name__ == "__main__":
