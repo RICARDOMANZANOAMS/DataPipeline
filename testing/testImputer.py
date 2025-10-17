a78f7bddd96c2bd49e42c58ce8f2def62cac24b9
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-10-17 08:26:09 -0400

    Add testMeadianImputer

--- testing/testImputer.py
+++ testing/testImputer.py
@@ -15,6 +15,13 @@ class testImputer(unittest.TestCase):
         mean_imputer=meanImputer()
         df_result=mean_imputer.impute(self.df,'a')
         pd.testing.assert_frame_equal(df_result, expected_df, check_dtype=False)
+
+    def testMedianImputer(self):
+        from imputer import medianImputer
+        expected_df=pd.DataFrame({'a':[4,5,6,5],'b':[3,4,5,6]})
+        median_imputer=medianImputer()
+        df_result=median_imputer.impute(self.df,'a')
+        pd.testing.assert_frame_equal(df_result,expected_df,check_dtype=False)
         
 if __name__ == "__main__":
     unittest.main()
