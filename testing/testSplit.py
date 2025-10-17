4ef01d7ce0da869f1b2de14e31da202c3a37479b
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-10-17 08:57:21 -0400

    Add testDataSplitStrategy

---
+++ testing/testSplit.py
@@ -0,0 +1,22 @@
+import unittest
+import pandas as pd
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+class testSplit(unittest.TestCase):
+    def setUp(self):
+        self.df=pd.DataFrame({'a':[1,2,3,4,5,6,7,8,9,10],'b':[11,12,13,14,15,16,17,18,19,20]})
+    
+    def testDataSplitStrategy(self):
+        from split import dataSplitStrategy
+        data_split_strategy=dataSplitStrategy()
+        result_array=data_split_strategy.splitDataset(self.df,0.3)
+        train_result,test_result=result_array[0]
+        expected_train_len =7
+        expected_test_len = 3
+        self.assertEqual(len(train_result), expected_train_len)
+        self.assertEqual(len(test_result), expected_test_len)
+   
+if __name__ == "__main__":
+    unittest.main()
