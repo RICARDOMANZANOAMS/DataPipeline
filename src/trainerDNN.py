7dddc0e4253be91299840a87ab14dc9008a810f2
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-10-20 23:07:38 -0400

    Add batch_size and shuffle to be configurable in create_dataloader. Add testing

--- src/trainerDNN.py
+++ src/trainerDNN.py
@@ -1,8 +1,8 @@
 from trainer import trainer
-class traineDNN(trainer):
+class trainerDNN(trainer):
     def __init__(self,df,featureNames,labelName):
         super().__init__(df,featureNames,labelName)
-        self.split
+        
 
     def split(self,splitMethod,params):
         from split import factorySplit       
@@ -19,15 +19,29 @@ class traineDNN(trainer):
             split_tensors.append((train_tensor,test_tensor))
         self.splitArray=split_tensors
             
-    def create_dataloader(self,df):
+    def create_dataloader(self,df,batch_size=32,shuffle=True):
         import torch
         from torch.utils.data import TensorDataset, DataLoader
         features=torch.tensor(df[self.featureNames].to_numpy())
         label=torch.tensor(df[self.labelName].to_numpy())
         tensor_dataset=TensorDataset(features,label)
-        dataloader_dataset=DataLoader(tensor_dataset,batch_size=32)
+        dataloader_dataset=DataLoader(tensor_dataset,batch_size=batch_size,shuffle=True)
         return dataloader_dataset
-
-
-   
     
+
+    
+import numpy as np
+import pandas as pd
+
+x=np.random.rand(30,4)
+y=np.random.choice([1,0],size=30)    
+feature_names=['f1','f2','f3','f4']
+X=pd.DataFrame(x,columns=feature_names)
+y=pd.DataFrame(y,columns=['label'])
+print(X)
+print(y)
+dataset=pd.concat([X,y],axis=1)
+print(dataset)
+trainer_dnn=trainerDNN(dataset,feature_names,'label')
+dataloader=trainer_dnn.create_dataloader(dataset)
+print(dataloader)
