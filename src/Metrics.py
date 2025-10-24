0248d97bb739a43397b3505de56f73853258ec07
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-10-24 19:56:25 -0400

    Add comments to metrics class

--- src/Metrics.py
+++ src/Metrics.py
@@ -7,25 +7,22 @@ class Metrics:
         self.dict_encode_labels=dict_encode_labels
         self.label_encoded=list(self.dict_encode_labels.values())
 
-    def calculate_metrics(self):
+    def calculate_metrics_classification(self):
+        '''
+        This method calculates the main important metrics for classification
+
+        '''
+        #Calculates the confussion matrix. Labels allows to order the label in the order of the array
         cm=confusion_matrix(self.ground_truth,self.predicted_values,labels=self.label_encoded)
-        main_metrics={}
-        for label,label_encoded in self.dict_encode_labels.items():
-            tp = cm[label_encoded, label_encoded]
-            fp = sum(cm[:, label_encoded]) - tp
-            fn = sum(cm[label_encoded, :]) - tp
-            tn= cm.sum() - (tp+ fp + fn)
-            precision = tp / (tp + fp)  # 10 / 13 ≈ 0.769
-            recall = tp / (tp + fn)     # 10 / 12 ≈ 0.833
-            f1 = 2 * (precision * recall) / (precision + recall)  # ≈ 0.8
+        main_metrics={}  #Stores each label with all the metrics
+        for label,label_encoded in self.dict_encode_labels.items(): #Iterate through each label
+            tp = cm[label_encoded, label_encoded]  #Calculate true positives
+            fp = sum(cm[:, label_encoded]) - tp    #Calculate false positives
+            fn = sum(cm[label_encoded, :]) - tp    #Calculate false negatives
+            tn= cm.sum() - (tp+ fp + fn)           #Calculate true negatives
+            precision = tp / (tp + fp)    #Precision
+            recall = tp / (tp + fn)       #Recall
+            f1 = 2 * (precision * recall) / (precision + recall)  #F1-score
 
             main_metrics[label]={"tp":tp,"fp":fp,"fn":fn,"tn":tn,"precision":precision,"recall":recall,"f1-score":f1}
         return main_metrics
-
-pred = [0, 1, 1, 2, 2, 2]
-true = [0, 0, 1, 2, 2, 1]
-encode = {"class0": 0, "class1": 1, "class2": 2}
-
-m = Metrics(pred, true, encode)
-results = m.calculate_metrics()
-print(results)
