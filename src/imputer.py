1fb9622c4ec07e444c8670a8badc24914b19c24f
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-11-03 23:46:08 -0500

    Fix logger error in factory and change classes to camel case notation

--- src/imputer.py
+++ src/imputer.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 from Logger import Logger
-class imputerStrategy(ABC):
+import logging
+class ImputerStrategy(ABC):
     '''
     This class is the abstract method to create imputers which can be mean, median, None
     Args:
@@ -16,7 +17,7 @@ class imputerStrategy(ABC):
         pass
         
 
-class meanImputer(imputerStrategy):
+class MeanImputer(ImputerStrategy):
     '''
     Child class of imputerStrategy that replaces none values with the mean of the feature
     
@@ -34,7 +35,7 @@ class meanImputer(imputerStrategy):
             raise TypeError(f"Feature '{featureName}' is not numeric, cannot impute with mean.") #Raise error if feature not numeric
     
 
-class medianImputer(imputerStrategy):
+class MedianImputer(ImputerStrategy):
     '''
     Child class of imputerStrategy that replaces null values with the median
     '''
@@ -50,7 +51,7 @@ class medianImputer(imputerStrategy):
         return df
        
 
-class valueNullImputer(imputerStrategy):
+class ValueNullImputer(ImputerStrategy):
     '''
     Child class of imputerStrategy that replaces null values with a value pass to impute
     '''
@@ -60,10 +61,10 @@ class valueNullImputer(imputerStrategy):
             raise ValueError("You must provide a replacementValue for valueImputer.")
         
         df[featureName] = df[featureName].fillna(replacementValue)
-        self.logger.info("Imputed columng {featureName} with value {replacementValue}")
+        self.logger.info(f"Imputed columng {featureName} with value {replacementValue}")
         return df
     
-class valueToNullImputer(imputerStrategy):
+class ValueToNullImputer(ImputerStrategy):
     '''
     Child class of imputerStrategy that replaces a value chosen by null
     '''
@@ -72,37 +73,48 @@ class valueToNullImputer(imputerStrategy):
         import numpy as np
         featureCol = df[featureName]
         df[featureName] = featureCol.replace(replacementValue, np.nan)
-        self.logger.info("Replace value {replacementValue} in column {featureName} with Null")
+        self.logger.info(f"Replace value {replacementValue} in column {featureName} with Null")
         return df
     
     
-class factoryImputer:
+class FactoryImputer:
     '''
     Factory class to implement all the classes in a modular way
     '''
     @staticmethod
-    def selectImputeMethod(imputeMethod):
-        logger = logger()  # Get the singleton logger
+    def selectImputeMethod(imputeMethod,logger):
+        
         try:
             if imputeMethod=="mean":
-                return meanImputer()
+                return MeanImputer(logger)
             elif imputeMethod=="median":
-                return medianImputer()
+                return MedianImputer(logger)
             elif imputeMethod=="value":
-                return valueNullImputer()
+                return ValueNullImputer(logger)
             elif imputeMethod=="valueToNull":
-                return valueToNullImputer()
+                return ValueToNullImputer(logger)
             else:
                 raise
         except Exception as e:
             logger.error("Error selecting impute method ", exc_info=True)
             return None
         
-# import pandas as pd
-# import numpy as np
-# path='./data/DDoS-ACK_Fragmentation.csv'
-# df=pd.read_csv(path)
-# featureName="Rate"
+import pandas as pd
+import numpy as np
+
+
+logger=Logger("app")
+logger.create_handler_with_level_and_format("info", "%(asctime)s - %(levelname)s - %(message)s","file",filename="app.log")
+log = logger.logger
+log.setLevel(logging.DEBUG)
+
+path='./data/DDoS-ACK_Fragmentation.csv'
+df=pd.read_csv(path)
+featureName="Rate"
+replacementValue=np.inf
+imputer=FactoryImputer.selectImputeMethod("valueToNull",log)
+df = imputer.impute(df, featureName, replacementValue=np.inf)
+print(df)
 # replacementValue=np.inf
 # objfactoryImputer=factoryImputer.selectImputeMethod("valueToNull")
 # df_new=objfactoryImputer.impute(df,featureName,replacementValue)
