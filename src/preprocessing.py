4dfc3ac7c847894534fa031a23ac933e1b25faa9
healthcanadacosmetics <ricardoamsrams@gmail.com>
2025-11-14 23:08:26 -0500

    Add Logger decorator to preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,24 +1,29 @@
+from Logger import Logger
 class preprocessing:
     '''
     Class that includes all the class for preprocessing such as dropDuplicates and imputeValues
     '''
     def __init__(self,df):
         self.df=df  
+        self.logger=Logger().get_logger()
 
+    @Logger.log_exceptions(lambda self: self.logger)
     def dropDuplicates(self):
         '''
         Method to drop duplicates in the dataset
-        '''
-        self.df=self.df.drop_duplciates()
-                
+        '''       
+        df=self.df.drop_duplicates()
+        return df
+        
+    @Logger.log_exceptions(lambda self: self.logger)            
     def imputeValue(self,imputeMethod,featureName,replacementValue):
         '''
         Method to impute values in an specific feature in the dataset
         '''
         from imputer import factoryImputer
         objFactoryImputer=factoryImputer.selectImputeMethod(imputeMethod)
-        objFactoryImputer.impute(self.df,featureName, replacementValue=None)
-        
+        df=objFactoryImputer.impute(self.df,featureName, replacementValue=None)
+        return df
 
     
     
